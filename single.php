75ad4a23ff503d8cae8f13547dd5c74fcd027776
WINNERAS <winneras@gmail.com>
2014-06-10 10:02:34 +0100

    remove commont

--- single.php
+++ single.php
@@ -10,11 +10,11 @@
 						
 						<header>
 						
-							<?php the_post_thumbnail( 'wpbs-featured' ); ?>
+							<?php //the_post_thumbnail( 'wpbs-featured' ); ?>
 							
 							<div class="page-header"><h1 class="single-title" itemprop="headline"><?php the_title(); ?></h1></div>
 							
-							<p class="meta"><?php _e("Posted", "wpbootstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php _e("by", "wpbootstrap"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "wpbootstrap"); ?> <?php the_category(', '); ?>.</p>
+							<p class="meta"><?php _e("Posted", "wpbootstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php// _e("by", "wpbootstrap"); ?> <?php// the_author_posts_link(); ?>  <?php // _e("filed under", "wpbootstrap"); ?> <?php the_category(', '); ?>.</p>
 						
 						</header> <!-- end article header -->
 					
@@ -40,7 +40,7 @@
 					
 					</article> <!-- end article -->
 					
-					<?php comments_template('',true); ?>
+					<?php // comments_template('',true); ?>
 					
 					<?php endwhile; ?>			
 					
